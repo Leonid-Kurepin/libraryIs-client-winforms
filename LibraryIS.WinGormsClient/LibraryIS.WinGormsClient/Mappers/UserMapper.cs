92e371c825752768dbd8af6d7078f8c91589ffc9
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-20 10:43:46 +0400

    Add UserMapper class
    This class is aimed to map from UserDto to UserView

---
+++ LibraryIS.WinGormsClient/LibraryIS.WinGormsClient/Mappers/UserMapper.cs
@@ -0,0 +1,20 @@
+﻿using LibraryIS.WinFormsClient.Dto;
+using LibraryIS.WinFormsClient.View;
+
+namespace LibraryIS.WinFormsClient.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserView MapToUserView(UserDto userDto)
+        {
+            return new UserView()
+            {
+                Id =  userDto.Id,
+                Name = userDto.Name,
+                Email =  userDto.Email,
+                Password =  userDto.Password,
+                Role =  userDto.Role
+            };
+        }
+    }
+}
