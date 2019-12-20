0199b6e6ea2536684c7a6a7f72ce9c2d4e64e5ed
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-20 10:42:42 +0400

    Add UsersRequestsExtension class for LibraryIsHttpClient

---
+++ LibraryIS.WinGormsClient/LibraryIS.WinGormsClient/HttpClient/Extensions/UsersRequestsExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using LibraryIS.WinFormsClient.Dto;
+
+namespace LibraryIS.WinFormsClient.HttpClient.Extensions
+{
+    public static class UsersRequestsExtension
+    {
+        public static async Task<PagedCollectionResponse<UserDto>> GetUsers(this LibraryIsHttpClient client)
+        {
+            //HTTP GET
+            var response = await client.GetAsync("api/users");
+
+            return  await response.Content.ReadAsAsync<PagedCollectionResponse<UserDto>>();
+        }
+    }
+}
